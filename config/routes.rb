904e3a698c9a328a03635be15be15e9f66462dc1
justin <davegle86@gmail.com>
2017-04-27 22:55:08 +0800

    move css to application & arrange our client section

--- config/routes.rb
+++ config/routes.rb
@@ -16,14 +16,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :jobs do
     resources :resumes
-      collection do
-        get :search
-      end
+    collection do
+      get :search
     end
+  end
 
-
-
-
+  
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
