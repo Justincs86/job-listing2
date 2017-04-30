9a38e4f4b43d1276049b78c3773ee8bd473e6f2e
justin <davegle86@gmail.com>
2017-04-30 17:55:14 +0800

    add admin function to company

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   def admin?
-     email == 'davegle@gmail.com'
+     is_admin
   end
 
   has_many :resumes
