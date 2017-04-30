ec7e0be996bde1674c98d773dc90d98b7ef3f263
justin <davegle86@gmail.com>
2017-04-30 12:03:39 +0800

    implement devise signup/login page

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users
-  
+  devise_for :users, :controllers => { :registration => "users/registrations"}
+
   resources :jobs do
     resources :resumes
     collection do
