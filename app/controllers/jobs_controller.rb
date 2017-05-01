deb0720625ba7bdfc7b2b9809391e14f73623917
justin <davegle86@gmail.com>
2017-05-01 23:45:44 +0800

    arrange jobs/index page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,14 +5,12 @@ class JobsController < ApplicationController
   def index
     @jobs = case params[:order]
             when 'by_lower_bound'
-              Job.published.order('wage_lower_bound DESC')
+              Job.published.order('wage_lower_bound DESC').paginate(:page => params[:page], :per_page => 15)
             when 'by_upper_bound'
-              Job.published.order('wage_upper_bound DESC')
+              Job.published.order('wage_upper_bound DESC').paginate(:page => params[:page], :per_page => 15)
             else
-              Job.published.order('created_at DESC')
+             Job.published.recent.paginate(:page => params[:page], :per_page => 15)
             end
-       @q = Job.ransack(params[:q])
-@products = @q.result(distinct: true)
 
   end
 
@@ -85,7 +83,7 @@ class JobsController < ApplicationController
 
 
 
-  
+
   private
 
   def job_params
--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users, :controllers => { :registration => "users/registrations"}
-
+  devise_for :users
+  
   resources :jobs do
     resources :resumes
     collection do
