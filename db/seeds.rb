34ee4ad9f5800523a3a8a3b559c1ad88603a22fc
justin <davegle86@gmail.com>
2017-05-02 16:16:29 +0800

    rake db:seed create jobs

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,53 @@
 puts 'Hello World!'
 puts '這個種子檔會自動建立一個帳號, 並且随机創建 10 個jobs，10个隐藏的jobs'
 
-create_account = User.create([email: 'example@gmail.com', password: '12345678',
+create_account = User.create([
+  email: 'example@gmail.com', password: '12345678',
   password_confirmation: '12345678', is_admin: 'true'])
 puts 'Admin account is created successfully!'
+
+
+
+
+for i in 1..10 do
+
+                Job.create!([
+                category: "Technology",
+                company: "Smartisan",
+                location: "Beijing, China",
+                title: "Software Engineer",
+                description: "Developing and maintain current or new software structure for new project ",
+                wage_lower_bound: rand(10..20) * 1000,
+                wage_upper_bound: rand(20..45) * 1000,
+                is_hidden: "false"
+                ])
+              end
+
+for i in 1..10 do
+
+              Job.create!([
+                category: "Technology",
+                company: "Xiaomi",
+                location: "Beijing, China",
+                title: "Senior Software Engineer",
+                description: "Developing and maintain current or new software structure for new project ",
+                wage_lower_bound: rand(10..20) * 1000,
+                wage_upper_bound: rand(20..50) * 1000,
+                is_hidden: "false"
+                ])
+              end
+
+
+for i in 1..10 do
+
+                Job.create!([
+                  category: "Business",
+                  company: "Google",
+                  location: "California, United States",
+                  title: " Senior Business Executive",
+                  description: "Business Development by organizing activities and training, monitoring, performance review and sales activities.",
+                  wage_lower_bound: rand(10..20) * 1000,
+                  wage_upper_bound: rand(20..50) * 1000,
+                  is_hidden: "false"
+                  ])
+                end
